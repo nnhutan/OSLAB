b03a39ebfb5df2d0b72aec761da118de625e72a0
NhuTanCS <tan.dinh1710@hcmut.edu.vn>
2021-05-21 20:24:19 +0700

    done lab 7!

---
+++ lab7/queue.h
@@ -0,0 +1,19 @@
+/////////// queue.h /////////////
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "structs.h"
+
+/* Initialize the process queue */
+void initialize_queue ( struct pqueue_t * q ) ;
+
+/* Get a process from a queue */
+struct pcb_t * de_queue( struct pqueue_t * q ) ;
+
+/* Put a process in to a queue */
+void en_queue(struct pqueue_t * q , struct pcb_t * proc ) ;
+
+int empty(struct pqueue_t * q ) ;
+
+#endif
---
+++ lab7/structs.h
@@ -0,0 +1,35 @@
+/////////// structs.h /////////////
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <pthread.h>
+
+/*The PCB of a process */
+struct pcb_t {
+	/* Values initialized for each process */
+	int arrival_time ; // The timestamp at which process arrives
+	// and wishes to start
+	int burst_time ; // The amount of time that process requires
+	// to complete its job
+	int pid ; // process id
+};
+
+/* ''Wrapper' of PCB in a queue*/
+struct qitem_t {
+	struct pcb_t * data ;
+	struct qitem_t * next ;
+};
+
+/* The 'queue' used for both ready queue and in_queue (e.g. the list of
+ * processes that will be loaded in the future ) */
+struct pqueue_t {
+	/* HEAD and TAIL for queue */
+	struct qitem_t * head ;
+	struct qitem_t * tail ;
+	/* MUTEX used to pro tec t the queue from
+	   be ing modified by mul t iple threads */
+	pthread_mutex_t lock ;
+};
+
+
+#endif
